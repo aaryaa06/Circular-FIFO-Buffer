9bfd98d5b12c56b4c2969b2b7be28c2154a3c202
aaryaa06 <aaryaanb06@gmail.com>
2025-07-17 22:04:02 +0530

    Update fifo.v

--- fifo.v
+++ fifo.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 15.07.2025 17:46:12
-// Design Name: 
-// Module Name: fifo
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module fifo #(
     parameter DATA_WIDTH = 16,
