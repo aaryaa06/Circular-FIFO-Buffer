bc2d4c770701ba754588ffb3cd8a59036959a136
aaryaa06 <aaryaanb06@gmail.com>
2025-07-17 22:03:42 +0530

    Update fifo_tb.v

--- fifo_tb.v
+++ fifo_tb.v
@@ -1,24 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 15.07.2025 17:59:34
-// Design Name: 
-// Module Name: fifo_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 `timescale 1ns/1ps
 
